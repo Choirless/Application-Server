45bfba64deaa9b68ad3f96834069cacff18e9390
Sean Tracey <seanmtracey@gmail.com>
2020-05-03 03:02:12 +0100

    Get video feed

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
 
 // view engine setup
@@ -36,8 +33,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index'));
+app.use('/create', require('./routes/create'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
