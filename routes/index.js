f7d7eccc73e28d4d7c96581b587d9596881ed836
Sean Tracey <seanmtracey@gmail.com>
2020-05-27 01:17:13 +0100

    Basic styles for dashboard

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: "Choirless | All the world's a stage", bodyid: "home" });
 });
 
+router.get('/dashboard', (req, res, next) => {
+	res.render('dashboard', { title : "Choirless | My Dashboard", bodyid: "dashboard" } );
+});
+
 module.exports = router;
