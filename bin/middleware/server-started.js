38dc681f8d4af16fa77216b01b23cac618ac561a
Sean Tracey <seanmtracey@gmail.com>
2020-07-08 16:51:32 +0100

    Tiny debug message;

--- bin/middleware/server-started.js
+++ bin/middleware/server-started.js
@@ -1,6 +1,8 @@
 const debug = require('debug')('bin:middleware:server-started');
 const serverStarted = Number(Date.now());
 
+debug('serverStarted:', serverStarted);
+
 module.exports = (req, res, next) => {
     res.locals.serverStarted = serverStarted;
     next();
