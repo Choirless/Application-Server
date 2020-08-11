b625082644e47a1f6e81a113d550400c93ca1f61
Sean Tracey <seanmtracey@gmail.com>
2020-08-11 04:19:08 +0100

    Allow super users to impersonate regular users for debugging

--- bin/middleware/check-session.js
+++ bin/middleware/check-session.js
@@ -1,8 +1,9 @@
 const debug = require('debug')('bin:middleware:check-session');
 
 module.exports = (req, res, next) => {
-    res.locals.user = req.session.user;
+    res.locals.user = req.session.impersonatedId || req.session.user;
     res.locals.name = req.session.name;
     res.locals.userType = req.session.userType;
+    res.locals.impersonating = req.session.impersonating;
     next();
 };
